acfc20d89e0cb5235077f01d54eb895311409f7e
Brad Pitcher <bradpitcher@gmail.com>
2014-05-03 14:33:53 -0700

    soft code the receipt link

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,7 @@ class MainHandler(webapp2.RequestHandler):
     self.response.write('Hello world!')
 
 
-def send_thank_you(email, pledge_id):
+def send_thank_you(email, host_url, pledge_id):
   """ Deferred email task """
 
   sender = 'MayOne no-reply <noreply@pure-spring-568.appspotmail.com>'
@@ -53,9 +53,10 @@ def send_thank_you(email, pledge_id):
   message = mail.EmailMessage(sender=sender, subject=subject)
   message.to = email
   format_kwargs = {
+    # TODO: Use the person's actual name
     'name': email,
-    # TODO: Softcode the receipt link
-    'receipt_link': 'http://localhost:8080/pledge/%s' % pledge_id
+    # TODO: write a handler for this
+    'receipt_link': '{0}/pledge/{1}'.format(host_url, pledge_id)
   }
   message.body = open('email/thank-you.txt').read().format(**format_kwargs)
   message.html = open('email/thank-you.html').read().format(**format_kwargs)
@@ -101,7 +102,10 @@ class PledgeHandler(webapp2.RequestHandler):
     pledge.save()
 
     # Add thank you email to a task queue
-    deferred.defer(send_thank_you, email, pledge.key().id(), _queue="mail")
+    deferred.defer(send_thank_you, email,
+                   self.request.host_url,
+                   pledge.key().id(),
+                   _queue="mail")
 
     self.response.write('Ok.')
 
