5b986c81d22d48e44f59ca1c3c5564f312918def
hjfreyer <yt@hjfreyer.com>
2014-05-03 12:10:09 -0400

    Added stripe integration, basic pledge page

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,98 +1,99 @@
 module.exports = function(grunt) {
-  // configure the tasks
-  grunt.initConfig({
-    copy: {
-      main: {
-        files: [
-          {cwd: 'backend/', src: '**', dest: 'build/', expand: true },
-          {cwd: 'assets/', src: '**', dest: 'build/static/', expand: true },
-        ],
-      },
-    },
-
-    clean: {
-      main: {
-        src: [ 'build' ],
-      },
-    },
-
-    sass: {
-      main: {
-        files: {
-          'build/static/style.css': 'stylesheets/style.scss'
-        }
-      }
-    },
-
-jade: {
-  compile: {
-    options: {
-      data: {
-        debug: false
-      }
-    },
-    files: {
-      "build/static/index.html": "markup/index.jade"
-    }
-  }
-},
-
-    autoprefixer: {
-      build: {
-        expand: true,
-        cwd: 'build/static/css/',
-        src: [ '*.css' ],
-        dest: 'build/static/css/'
-      }
-    },
-
-    shell: {
-      devserver: {
-        command: 'dev_appserver.py build/',
-        options: {
-          async: true,
+    // configure the tasks
+    grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                    {cwd: 'backend/', src: '**', dest: 'build/', expand: true },
+                    {cwd: 'assets/', src: '**', dest: 'build/static/', expand: true },
+                ],
+            },
         },
-      },
-    },
 
-    watch: {
-      stylesheets: {
-        files: 'stylesheets/**',
-        tasks: [ 'css' ]
-      },
-      markup: {
-        files: 'markup/**',
-        tasks: [ 'jade' ]
-      },
-      copy: {
-        files: [ '{js/src,resources,assets,templates,backend}/**' ],
-        tasks: [ 'copy' ]
-      }
-    },
-  });
+        clean: {
+            main: {
+                src: [ 'build' ],
+            },
+        },
 
-  // load the tasks
-  grunt.loadNpmTasks('grunt-autoprefixer');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-jade');
-  grunt.loadNpmTasks('grunt-shell-spawn');
+        sass: {
+            main: {
+                files: {
+                    'build/static/style.css': 'stylesheets/style.scss'
+                }
+            }
+        },
 
-  // define the tasks
-  grunt.registerTask(
-    'css', '',
-    [ 'sass', 'autoprefixer' ]
-  );
-  grunt.registerTask(
-    'build',
-    'Compiles all of the assets and copies the files to the build directory.',
-    [ 'clean', 'copy', 'css', 'jade']
-  );
-  grunt.registerTask(
-    'dev',
-    'Builds, runs the dev server, and watches for updates.',
-    [ 'build', 'shell:devserver', 'watch']
-  );
+        jade: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: {
+                    "build/static/index.html": "markup/index.jade",
+                    "build/static/pledge.html": "markup/pledge.jade"
+                }
+            }
+        },
+
+        autoprefixer: {
+            build: {
+                expand: true,
+                cwd: 'build/static/css/',
+                src: [ '*.css' ],
+                dest: 'build/static/css/'
+            }
+        },
+
+        shell: {
+            devserver: {
+                command: 'dev_appserver.py build/',
+                options: {
+                    async: true,
+                },
+            },
+        },
+
+        watch: {
+            stylesheets: {
+                files: 'stylesheets/**',
+                tasks: [ 'css' ]
+            },
+            markup: {
+                files: 'markup/**',
+                tasks: [ 'jade' ]
+            },
+            copy: {
+                files: [ '{js/src,resources,assets,templates,backend}/**' ],
+                tasks: [ 'copy' ]
+            }
+        },
+    });
+
+    // load the tasks
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-shell-spawn');
+
+    // define the tasks
+    grunt.registerTask(
+        'css', '',
+        [ 'sass', 'autoprefixer' ]
+    );
+    grunt.registerTask(
+        'build',
+        'Compiles all of the assets and copies the files to the build directory.',
+        [ 'clean', 'copy', 'css', 'jade']
+    );
+    grunt.registerTask(
+        'dev',
+        'Builds, runs the dev server, and watches for updates.',
+        [ 'build', 'shell:devserver', 'watch']
+    );
 };
--- backend/main.py
+++ backend/main.py
@@ -14,14 +14,55 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+import datetime
 import webapp2
 import urllib2
-proxy_support = urllib2.ProxyHandler({})
+from google.appengine.ext import db
+
+
+
+class Pledge(db.Model):
+  donationTime = db.DateTimeProperty(auto_now_add=True)
+  email = db.EmailProperty(required=True)
+  amountCents = db.IntegerProperty(required=True)
+  stripeToken = db.StringProperty(required=True)
+  note = db.TextProperty(required=False)
+
+  fundraisingRound = db.StringProperty(required=True)
+
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+  def get(self):
+    self.response.write('Hello world!')
+
+
+class PledgeHandler(webapp2.RequestHandler):
+  def get(self):
+    if not (self.request.get('token')
+            and self.request.get('amount')
+            and self.request.get('email')):
+      self.error(400)
+      self.response.write('Invalid request')
+      return
+
+    try:
+      amount = int(self.request.get('amount'))
+    except ValueError:
+      self.error(400)
+      self.response.write('Invalid request')
+      return
+
+    pledge = Pledge(email=self.request.get('email'),
+                    amountCents=amount,
+                    stripeToken=self.request.get('token'),
+                    note=self.request.get('note'),
+                    fundraisingRound="1")
+    pledge.save()
+    self.response.write('Ok.')
+
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+  ('/pledge.do', PledgeHandler),
+  ('/', MainHandler)
 ], debug=True)
