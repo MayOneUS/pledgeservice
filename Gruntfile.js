8bcb1cd39d812a26e0d459f89b081b17906db9ee
Hunter Freyer <yt@hjfreyer.com>
2014-05-05 21:51:18 -0400

    Clean up Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,108 +1,99 @@
 module.exports = function(grunt) {
-    // configure the tasks
-    grunt.initConfig({
-        copy: {
-            main: {
-                files: [
-                    {cwd: 'backend/', src: '**', dest: 'build/', expand: true },
-                    {cwd: 'assets/', src: '**', dest: 'build/static/', expand: true },
-                ],
-            },
+  // configure the tasks
+  grunt.initConfig({
+    copy: {
+      main: {
+        files: [
+          {cwd: 'backend/', src: '**', dest: 'build/', expand: true },
+          {cwd: 'assets/', src: '**', dest: 'build/static/', expand: true },
+        ],
+      },
+    },
+
+    clean: {
+      main: {
+        src: [ 'build' ],
+      },
+    },
+
+    sass: {
+      main: {
+        files: {
+          'build/static/style.css': 'stylesheets/style.scss'
+        }
+      }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          data: {
+            debug: false
+          }
         },
+        files: [
+          {cwd: 'markup/', src: '*.jade', dest: 'build/static/', expand: true,
+           ext: '.html'},
+        ]
+      }
+    },
 
-        clean: {
-            main: {
-                src: [ 'build' ],
-            },
+    autoprefixer: {
+      build: {
+        expand: true,
+        cwd: 'build/static/css/',
+        src: [ '*.css' ],
+        dest: 'build/static/css/'
+      }
+    },
+
+    shell: {
+      devserver: {
+        command: 'dev_appserver.py --host 0.0.0.0 --admin_host 0.0.0.0 build/',
+        options: {
+          async: true,
         },
+      },
+    },
 
-        sass: {
-            main: {
-                files: {
-                    'build/static/style.css': 'stylesheets/style.scss'
-                }
-            }
-        },
+    watch: {
+      stylesheets: {
+        files: 'stylesheets/**',
+        tasks: [ 'css' ]
+      },
+      markup: {
+        files: 'markup/**',
+        tasks: [ 'jade' ]
+      },
+      copy: {
+        files: [ '{js/src,resources,assets,templates,backend}/**' ],
+        tasks: [ 'copy' ]
+      }
+    },
+  });
 
-        jade: {
-            compile: {
-                options: {
-                    data: {
-                        debug: false
-                    }
-                },
-                files: {
-                    "build/static/index.html": "markup/index.jade",
-                    "build/static/more.html": "markup/more.jade",
-                    "build/static/pledge.html": "markup/pledge.jade",
-                    "build/static/privacy.html": "markup/privacy.jade",
-                    "build/static/thankyou.html": "markup/thankyou.jade",
-                    "build/static/contact.html": "markup/contact.jade",
-                    "build/static/distribution.html": "markup/distribution.jade",
-                    "build/static/learnmore.html": "markup/learnmore.jade",
-                    "build/static/aboutus.html": "markup/aboutus.jade",
-                    "build/static/faq.html": "markup/faq.jade",
-                    "build/static/embed.html": "markup/embed.jade"
-                }
-            }
-        },
+  // load the tasks
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-shell-spawn');
 
-        autoprefixer: {
-            build: {
-                expand: true,
-                cwd: 'build/static/css/',
-                src: [ '*.css' ],
-                dest: 'build/static/css/'
-            }
-        },
-
-        shell: {
-            devserver: {
-                command: 'dev_appserver.py --host 0.0.0.0 --admin_host 0.0.0.0 build/',
-                options: {
-                    async: true,
-                },
-            },
-        },
-
-        watch: {
-            stylesheets: {
-                files: 'stylesheets/**',
-                tasks: [ 'css' ]
-            },
-            markup: {
-                files: 'markup/**',
-                tasks: [ 'jade' ]
-            },
-            copy: {
-                files: [ '{js/src,resources,assets,templates,backend}/**' ],
-                tasks: [ 'copy' ]
-            }
-        },
-    });
-
-    // load the tasks
-    grunt.loadNpmTasks('grunt-autoprefixer');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-sass');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-jade');
-    grunt.loadNpmTasks('grunt-shell-spawn');
-
-    // define the tasks
-    grunt.registerTask(
-        'css', '',
-        [ 'sass', 'autoprefixer' ]
-    );
-    grunt.registerTask(
-        'build',
-        'Compiles all of the assets and copies the files to the build directory.',
-        [ 'clean', 'copy', 'css', 'jade']
-    );
-    grunt.registerTask(
-        'dev',
-        'Builds, runs the dev server, and watches for updates.',
-        [ 'build', 'shell:devserver', 'watch']
-    );
+  // define the tasks
+  grunt.registerTask(
+    'css', '',
+    [ 'sass', 'autoprefixer' ]
+  );
+  grunt.registerTask(
+    'build',
+    'Compiles all of the assets and copies the files to the build directory.',
+    [ 'clean', 'copy', 'css', 'jade']
+  );
+  grunt.registerTask(
+    'dev',
+    'Builds, runs the dev server, and watches for updates.',
+    [ 'build', 'shell:devserver', 'watch']
+  );
 };
