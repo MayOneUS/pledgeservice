8e1f5bf84e8293b9a90f224352878d51f1b8a456
JT Olds <hello@jtolds.com>
2014-05-06 10:24:28 -0600

    tell stripe more info about each pledge

--- backend/main.py
+++ backend/main.py
@@ -125,7 +125,12 @@ class PledgeHandler(webapp2.RequestHandler):
       self.response.write('Invalid request: Bad email address')
       return
 
-    customer = stripe.Customer.create(card=token)
+    customer = stripe.Customer.create(card=token, email=email, metadata={
+        "amount": str(amount),
+        "occupation": occupation,
+        "employer": employer,
+        "phone": phone,
+        "target": target})
 
     pledge = model.addPledge(
             email=email, stripe_customer_id=customer.id, amount_cents=amount,
