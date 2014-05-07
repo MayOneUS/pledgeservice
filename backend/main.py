b1023e63cf33125a68e70de307124fdc9212ba59
Hunter Freyer <yt@hjfreyer.com>
2014-05-07 01:33:10 -0400

    Get rid of config_NOCOMMIT. There's no longer any secret data a dev needs to get going (in theory).

--- backend/main.py
+++ backend/main.py
@@ -9,8 +9,6 @@ from google.appengine.ext import db, deferred
 import stripe
 import model, wp_import
 
-import config_NOCOMMIT
-
 
 # This gets added to every pledge calculation
 BASE_TOTAL = 42368700
@@ -144,36 +142,36 @@ class PledgeHandler(webapp2.RequestHandler):
 
 
 class UserUpdateHandler(webapp2.RequestHandler):
-    def get(self, url_nonce):
-        user = model.User.all().filter("url_nonce =", url_nonce).get()
-        if user is None:
-            self.error(404)
-            self.response.write('This page was not found')
-            return
+  def get(self, url_nonce):
+    user = model.User.all().filter("url_nonce =", url_nonce).get()
+    if user is None:
+      self.error(404)
+      self.response.write('This page was not found')
+      return
 
-        template = JINJA_ENVIRONMENT.get_template('user-update.html')
-        self.response.write(template.render({'user': user}))
+    template = JINJA_ENVIRONMENT.get_template('user-update.html')
+    self.response.write(template.render({'user': user}))
 
-    def post(self, url_nonce):
-        try:
-            user = model.User.all().filter("url_nonce =", url_nonce).get()
-            if user is None:
-                self.error(404)
-                self.response.write('This page was not found')
-                return
+  def post(self, url_nonce):
+    try:
+      user = model.User.all().filter("url_nonce =", url_nonce).get()
+      if user is None:
+        self.error(404)
+        self.response.write('This page was not found')
+        return
 
-            user.occupation = self.request.get('occupation')
-            user.employer = self.request.get('employer')
-            user.phone = self.request.get('phone')
-            user.target = self.request.get('target')
-            user.put()
-            template = JINJA_ENVIRONMENT.get_template('user-update.html')
-            ctx = {'user': user, 'success': True}
-            self.response.write(template.render(ctx))
-        except:
-            self.error(400)
-            self.response.write('There was a problem submitting the form')
-            return
+      user.occupation = self.request.get('occupation')
+      user.employer = self.request.get('employer')
+      user.phone = self.request.get('phone')
+      user.target = self.request.get('target')
+      user.put()
+      template = JINJA_ENVIRONMENT.get_template('user-update.html')
+      ctx = {'user': user, 'success': True}
+      self.response.write(template.render(ctx))
+    except:
+      self.error(400)
+      self.response.write('There was a problem submitting the form')
+      return
 
 
 app = webapp2.WSGIApplication([
@@ -181,7 +179,7 @@ app = webapp2.WSGIApplication([
   ('/stripe_public_key', GetStripePublicKeyHandler),
   ('/pledge.do', PledgeHandler),
   ('/user-update/(\w+)', UserUpdateHandler),
-  ('/import.do', wp_import.ImportHandler),
-  ('/campaigns/may-one', EmbedHandler),
-  ('/campaigns/may-one/', EmbedHandler)
+  ('/campaigns/may-one/?', EmbedHandler)
+  # See wp_import
+  # ('/import.do', wp_import.ImportHandler),
 ], debug=False)
