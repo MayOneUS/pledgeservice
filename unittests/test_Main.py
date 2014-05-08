58d265a8e3f7193c957150aa90c37c8bbafe7335
Austin <austin.h.lee@gmail.com>
2014-05-08 11:07:07 -0700

    changed test file to be more generic (file focused not class focused)

--- unittests/test_Main.py
+++ unittests/test_Main.py
@@ -51,4 +51,18 @@ class TestUser(unittest.TestCase):
     
     #TODO: confirm storage in datastore.
 
+
+class TestPledge(unittest.TestCase):
+    def setUp(self):
+      # First, create an instance of the Testbed class.
+      self.testbed = testbed.Testbed()
+      # Then activate the testbed, which prepares the service stubs for use.
+      self.testbed.activate()
+      # Next, declare which service stubs you want to use.#self.testbed.init_datastore_v3_stub()
+      #self.testbed.init_memcache_stub()
+      #self.testbed.init_urlfetch_stub()
+
+    def tearDown(self):
+      self.testbed.deactivate()
+     
     
