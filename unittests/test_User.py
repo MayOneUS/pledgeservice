2be867af5f50b1070d88995d5cc4b0d54dec71ab
Austin <austin.h.lee@gmail.com>
2014-05-08 10:45:47 -0700

    tests for User class

--- unittests/test_User.py
+++ unittests/test_User.py
@@ -6,24 +6,49 @@ import logging
 from google.appengine.ext import ndb
 from google.appengine.ext import testbed
 
-from 
+from backend import main
 
 class TestUser(unittest.TestCase):
-    def setUp(self):
-        # First, create an instance of the Testbed class.
-        self.testbed = testbed.Testbed()
-        # Then activate the testbed, which prepares the service stubs for use.
-        self.testbed.activate()
-        # Next, declare which service stubs you want to use.
-        #self.testbed.init_datastore_v3_stub()
-        #self.testbed.init_memcache_stub()
-        #self.testbed.init_urlfetch_stub()
+  def setUp(self):
+    # First, create an instance of the Testbed class.
+    self.testbed = testbed.Testbed()
+    # Then activate the testbed, which prepares the service stubs for use.
+    self.testbed.activate()
+    # Next, declare which service stubs you want to use.#self.testbed.init_datastore_v3_stub()
+    #self.testbed.init_memcache_stub()
+    #self.testbed.init_urlfetch_stub()
 
-    def tearDown(self):
-        self.testbed.deactivate()
+  def tearDown(self):
+    self.testbed.deactivate()
+
+  def test_createOrUpdate(self):
+    '''Create or Update a User'''
+    fake_email = 'john@smith.com'
+    fake_stripe_id = 'should there be a test ID for proper testing?'
+    fake_occupation = 'Lobbyist'
+    fake_employer = 'Acme'
+    fake_phone = '800-555-1212'
+    fake_target = None
     
-    def test_createOrUpdate(self):
-        '''Create or Update a User'''
-        logging.info('Testing creation of a User')
-        
-        
+    logging.info('Testing updating a user that does not exist...')
+    
+    user0 = main.User()
+    user0.createOrUpdate(
+      email=fake_email, 
+      occupation = fake_occupation, 
+      employer = fake_employer, 
+      phone= fake_phone )
+      
+    self.assertEqual(user0.email, fake_email)
+    self.assertEqual(user0.occupation, fake_occupation)
+    self.assertEqual(user0.employer, fake_employer)
+    self.assertEqual(user0.phone, fake_phone)
+    
+    logging.info('Test updating that user we just created...')
+    user1 = main.User()
+    user1.createOrUpdate(email=fake_email, fake_occupation='Regulator')
+    self.assertEqual(user1.fake_occupation, 'Regulator')
+    
+    #TODO: confirm storage in datastore.
+
+    
