fa55012096362e469ae6703308d4433b3862d83c
Austin <austin.h.lee@gmail.com>
2014-05-14 11:16:27 -0700

    covered 4 space indent to 2 space indent

--- testrunner.py
+++ testrunner.py
@@ -8,7 +8,6 @@ Run unit tests for App Engine apps."""
 SDK_PATH_manual = '/usr/local/google_appengine'
 TEST_PATH_manual = 'unittests'
 
-
 def main(sdk_path, test_path):
   sys.path.insert(0, sdk_path)
   sys.path.insert(1,'backend')
@@ -24,5 +23,5 @@ if __name__ == '__main__':
   TEST_PATH = TEST_PATH_manual
   SDK_PATH_override = raw_input('Enter SDK_PATH Override (Leave blank to use the default): ')
   if SDK_PATH_override:
-      SDK_PATH = SDK_PATH_override
+    SDK_PATH = SDK_PATH_override
   main(SDK_PATH, TEST_PATH)
