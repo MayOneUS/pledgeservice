01b44b17a267c8674d9b34db9f6afc5ae863e2fd
aaronlifshin <aaronlifshin@gmail.com>
2014-05-15 11:52:13 -0700

    Updated check total.  Updated chat link.

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ import wp_import
 PRE_SHARDING_TOTAL = 27425754  # See model.ShardedCounter
 WP_PLEDGE_TOTAL = 41326868
 DEMOCRACY_DOT_COM_BALANCE = 6478063
-CHECKS_BALANCE = 2547500  # lol US government humor
+CHECKS_BALANCE = 7655200  # lol US government humor
 
 
 class Error(Exception): pass
