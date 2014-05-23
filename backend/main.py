bec54c8bf8b5d24374c7862596427037ee64cf25
aaronlifshin <aaronlifshin@gmail.com>
2014-05-23 13:39:16 -0700

    Updated democracy.com balance

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ import wp_import
 # These get added to every pledge calculation
 PRE_SHARDING_TOTAL = 27425754  # See model.ShardedCounter
 WP_PLEDGE_TOTAL = 41326868
-DEMOCRACY_DOT_COM_BALANCE = 8553428
+DEMOCRACY_DOT_COM_BALANCE = 9036173
 CHECKS_BALANCE = 7655200  # lol US government humor
 
 
