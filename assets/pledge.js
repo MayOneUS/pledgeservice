beb70cc405b0510eeee0d2b1dbe82c6b1ea7b187
Hunter Freyer <yt@hjfreyer.com>
2014-05-28 20:43:05 -0400

    Frontend changes for pledging to a team.
    All we do is pass through the team from the query parameter. Perhaps
    customizing the form could happen eventually.

--- assets/pledge.js
+++ assets/pledge.js
@@ -1,3 +1,22 @@
+// Part of this function modified from here:
+// http://stackoverflow.com/a/2880929/1569632
+//
+// NOTE: This could be made more efficient by caching the result (and then
+// killing the cache on URL changes, but we only call it once, so meh.
+var getUrlParams = function() {
+  var match,
+  pl     = /\+/g,  // Regex for replacing addition symbol with a space
+  search = /([^&=]+)=?([^&]*)/g,
+  decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+  query  = window.location.search.substring(1);
+
+  urlParams = {};
+  while (match = search.exec(query))
+    urlParams[decode(match[1])] = decode(match[2]);
+
+  return urlParams;
+};
+
 var PledgeController = ['$scope', '$http', function($scope, $http) {
   $scope.ctrl = {
     paymentConfig: null,
@@ -30,6 +49,8 @@ var PledgeController = ['$scope', '$http', function($scope, $http) {
                                { STRIPE: { token: token.id } });
     },
     createPledge: function(name, payment) {
+      var urlParams = getUrlParams();
+
       $http.post('/r/pledge', {
         email: $scope.ctrl.form.email,
         phone: $scope.ctrl.form.phone,
@@ -39,7 +60,7 @@ var PledgeController = ['$scope', '$http', function($scope, $http) {
         target: $scope.ctrl.form.target,
         subscribe: $scope.ctrl.form.subscribe,
         amountCents: $scope.ctrl.cents(),
-        team: '',
+        team: urlParams['team'] || '',
         payment: payment
       }).success(function(data) {
         location.href = data.receipt_url;
