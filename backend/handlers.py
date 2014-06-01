dd97ef2e381b17a4ec2b8281c8540fbbab857605
Austin <austin.h.lee@gmail.com>
2014-06-01 13:50:19 -0700

    fixes

--- backend/handlers.py
+++ backend/handlers.py
@@ -195,37 +195,40 @@ class SubscribeHandler(webapp2.RequestHandler):
   def post(self):
     env = self.app.config['env']
     logging.info('body: %s' % self.request.body)
-    email_input = cgi.escape(self.request.get('email', default_value=None))
-    if email_input is None or len(email_input)==0:
+    email_input = cgi.escape(self.request.get('email'))
+    if len(email_input) == 0:
       logging.warning("Bad Request: required field (email) missing.")
       self.error(400)
       return
 
-    first_name = cgi.escape(self.request.get('first_name', default_value=None))
-    last_name = cgi.escape(self.request.get('last_name', default_value=None))
+    first_name = cgi.escape(self.request.get('first_name'))
+    if len(first_name) == 0:
+      first_name = None
 
-    zipcode_input = cgi.escape(self.request.get('zip', default_value=None))
-    if zipcode_input == '':
+    last_name = cgi.escape(self.request.get('last_name'))
+    if len(last_name) == 0:
+      last_name = None
+
+    zipcode_input = cgi.escape(self.request.get('zipcode'))
+    if len(zipcode_input) == 0:
       zipcode_input = None
-    
-    volunteer_input = cgi.escape(self.request.get('volunteer', default_value="")) # "YES" or "NO"
-    if volunteer_input=='on':
+
+    volunteer_input = cgi.escape(self.request.get('volunteer')) # "YES" or "NO"
+    if volunteer_input == 'on':
       volunteer_input = 'Yes'
-    elif volunteer_input=='off':
+    elif volunteer_input == 'off':
       volunteer_input = ''
-    
-    skills_input = cgi.escape(self.request.get('skills', default_value=None)) #Free text, limited to 255 char
-    if skills_input =='':
+
+    skills_input = cgi.escape(self.request.get('skills')) #Free text, limited to 255 char
+    if len(skills_input) == 0:
       skills_input = None
-    
-    rootstrikers_input = cgi.escape(self.request.get('rootstrikers', default_value='')) #Free text, limited to 255 char
+
+    rootstrikers_input = cgi.escape(self.request.get('rootstrikers')) #Free text, limited to 255 char
     if rootstrikers_input=='on':
       rootstrikers_input = 'Yes'
     elif rootstrikers_input=='off':
       rootstrikers_input = ''
-    
-    #TODO: rootstrikers (Waiting on details from Aaron re Mailchimp field update)
-    
+
     env.mailing_list_subscriber.Subscribe(
       email=email_input,
       first_name=first_name, last_name=last_name,
