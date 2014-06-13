27f221e6243fa832edc5b5a38d8863e71bbbf670
Austin <austin.h.lee@gmail.com>
2014-06-12 17:38:09 -0700

    added phone field to Subscribe handler and mail chimp subscription functions

--- backend/handlers.py
+++ backend/handlers.py
@@ -230,6 +230,10 @@ class SubscribeHandler(webapp2.RequestHandler):
     if len(last_name) == 0:
       last_name = None
 
+    phone_input = cgi.escape(self.request.get('phone'))
+    if len(phone_input) == 0:
+      phone_input = None
+
     zipcode_input = cgi.escape(self.request.get('zipcode'))
     if len(zipcode_input) == 0:
       zipcode_input = None
@@ -261,6 +265,7 @@ class SubscribeHandler(webapp2.RequestHandler):
       ip_addr=self.request.remote_addr,
       time=datetime.datetime.now(),
       source=source_input,
+      phone=phone_input,
       zipcode=zipcode_input,
       volunteer=volunteer_input,
       skills=skills_input,
