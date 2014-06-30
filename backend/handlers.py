c82eadb2df7d18f67f952f3fcbb6c32aeb505e60
Jon Parker <jparker165@gmail.com>
2014-06-30 13:47:38 -0700

    how did this typo get here? theyve all gone to pledgdev anyways

--- backend/handlers.py
+++ backend/handlers.py
@@ -608,7 +608,7 @@ class BitcoinStartHandler(webapp2.RequestHandler):
     post_data = {
       'posData': temp_key_str,
       'price': price_in_dollars,
-      'notificationURL': "https://pledge.mayday.us/r/bitcoin_notitifcations",
+      'notificationURL': "https://pledge.mayday.us/r/bitcoin_notifcations",
       'currency': 'USD',
       # 'buyerName': data["name"],
       # 'buyerEmail': data["email"]
