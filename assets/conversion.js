b5325dd481ae1c6ce9945c8c6918aba25cb25f9f
aaronlifshin <aaronlifshin@gmail.com>
2014-07-02 17:25:18 -0700

    Add conversino tracking for google ads post paypal

---
+++ assets/conversion.js
@@ -0,0 +1,18 @@
+<!-- Google Code for Receipt adwords Conversion Page -->
+<script type="text/javascript">
+/* <![CDATA[ */
+var google_conversion_id = 968755289;
+var google_conversion_language = "en";
+var google_conversion_format = "3";
+var google_conversion_color = "ffffff";
+var google_conversion_label = "VQjxCOfX9xwQ2ZD4zQM";
+var google_remarketing_only = false;
+/* ]]> */
+</script>
+<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+</script>
+<noscript>
+<div style="display:inline;">
+<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/968755289/?label=VQjxCOfX9xwQ2ZD4zQM&amp;guid=ON&amp;script=0"/>
+</div>
+</noscript>
