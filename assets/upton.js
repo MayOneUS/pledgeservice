976d94949559ec33f1b4b9b0ea186231b680843b
daniel <danielbporter@gmail.com>
2014-10-23 10:31:23 -0400

    copy changes, updating button

--- assets/upton.js
+++
@@ -1,5 +0,0 @@
-function SetTweet(){
-    var tweets = ["tweet1", "tweet2", "tweet3", "tweet4"]
-    var tweet = tweets[Math.random()*tweets.length|0]
-    document.getElementsByClassName('twitter')[0].href = "https://twitter.com/home?status=" + tweet;
-}
